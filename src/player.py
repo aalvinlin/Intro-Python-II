1fa3710767995bb7f8560af735408906df89325b
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-05 21:37:20 -0700

    start Player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,18 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+
+    self.inventory = []
+
+    def __init__(self):
+        self.room = 'outside'
+    
+    def drop_item(item):
+
+        # remove item if it exists
+        if self.inventory.index(item) >= 0:
+            self.inventory.remove(item)
+        
+        else
+            print("You don't have this in your inventory!")

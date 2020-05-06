8c9fe743d497e693d6c61379052d7ed5f999b3c0
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-05 21:28:25 -0700

    add Room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,36 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+
+    exits = {
+        "n": None,
+        "s": None,
+        "e": None,
+        "w": None,
+        "u": None,
+        "d": None
+    }
+
+    __init__(name, description):
+        self.name = name
+        self.description = description
+    
+    def n_to():
+        return exits['n']
+    
+    def s_to():
+        return exits['s']
+    
+    def e_to():
+        return exits['e']
+    
+    def w_to():
+        return exits['w']
+    
+    def u_to():
+        return exits['u']
+    
+    def d_to():
+        return exits['d']
+    
